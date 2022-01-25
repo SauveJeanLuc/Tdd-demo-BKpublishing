4698c36d963d0dd0585141345f67ffff91a4ae8c
Didien MUNEZERO <didiermunezer38@gmail.com>
2022-01-25 03:18:57 +0200

    feat(objects): base models

---
+++ src/main/java/models/Author.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class Author {
+    private Long id;
+    private String fullNames;
+    private LocalDateTime dob;
+
+    public Author() {
+    }
+
+    public Author(Long id, String fullNames, LocalDateTime dob) {
+        this.id = id;
+        this.fullNames = fullNames;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullNames() {
+        return fullNames;
+    }
+
+    public void setFullNames(String fullNames) {
+        this.fullNames = fullNames;
+    }
+
+    public LocalDateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/models/Book.java
@@ -0,0 +1,55 @@
+package models;
+
+import org.apache.tomcat.jni.Local;
+
+import java.time.LocalDateTime;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private LocalDateTime publishedAt;
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, LocalDateTime publishedAt, Author author) {
+        this.id = id;
+        this.title = title;
+        this.publishedAt = publishedAt;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDateTime publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
