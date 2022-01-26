ba025e94e5e9220e2c354a9b52d42039083b9cd0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-26 12:20:12 +0200

    Made DTO Cleaner with Project Lombok

--- src/main/java/com/example/books/dto/BookDto.java
+++ src/main/java/com/example/books/dto/BookDto.java
@@ -1,34 +1,12 @@
 package com.example.books.dto;
 
-import com.example.books.models.Author;
+import lombok.Data;
 
-import java.time.LocalDateTime;
-
+@Data
 public class BookDto {
+
     private String title;
+
     private Long authorId;
 
-    public BookDto() {
-    }
-
-    public BookDto( String title, LocalDateTime publishedAt, Long authorId) {
-        this.title = title;
-        this.authorId = authorId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Long getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(Long authorId) {
-        this.authorId = authorId;
-    }
 }
