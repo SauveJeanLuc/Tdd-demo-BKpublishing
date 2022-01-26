a5050af38d0aa9bf0d1cf9cce75cacb259cd31db
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-26 13:19:58 +0200

    Refactored data.sql to insert authors and books at the same time

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-insert into book(id, title,author) values(101,'Behind the evil ...',24);
-insert into book(id, title,author) values(101,'Things fall apart',24);
-insert into book(id, title,author) values(101,'Breaking the silence',24);
+insert into authors(id, full_names, date_of_birth) values(30, 'Julia Show', '1990-01-13');
+insert into authors(id, full_names, date_of_birth) values(31, 'Chinua Achebe', '1982-02-15');
+
+insert into books(id, title, author_id) values(101,'Behind the evil ...',30);
+insert into books(id, title, author_id) values(102,'Things fall apart',31);
+insert into books(id, title, author_id) values(103,'Breaking the silence',30);
